e912c1f142860f3f0415a6ef8b484a19d9074994
gusqls3329 <gusqls81017@gmail.com>
2024-04-05 20:19:51 +0900

    코드 1일차

---
+++ src/main/java/com/example/beenproject/eneities/CompulsionBoard.java
@@ -0,0 +1,26 @@
+package com.example.beenproject.eneities;
+
+import com.example.beenproject.eneities.enums.Reson;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CompulsionBoard {
+    @Id
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long iresolvedBoard;
+
+    @ManyToOne
+    @JoinColumn(name = "iuser", nullable = false)
+    private User user;
+
+    @Enumerated
+    private Reson reson;
+}
---
+++ src/main/java/com/example/beenproject/eneities/enums/Reson.java
@@ -0,0 +1,21 @@
+package com.example.beenproject.eneities.enums;
+
+import lombok.Getter;
+
+@Getter
+/*
+Suicide : 자살
+abuse : 욕설
+specific user : 특정 사용자 언급
+multiple accounts : 다중 계정
+impersonation : 사칭
+Hindrance : 방해
+ */
+public enum Reson {
+    SUICIDE,
+    ABUSE,
+    SPECIFICUSER,
+    MULTIACCOUNT,
+    IMPERSON,
+    HINDRAN;
+}
