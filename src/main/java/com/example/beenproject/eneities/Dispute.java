04686fcff82d48a63a9fb01e82025a471e477671
gusqls3329 <gusqls81017@gmail.com>
2024-04-05 20:07:13 +0900

    코드 1일차

--- src/main/java/com/example/beenproject/eneities/Dispute.java
+++ src/main/java/com/example/beenproject/eneities/Dispute.java
@@ -35,6 +35,4 @@ public class Dispute extends CreatedAt {
 
     @Enumerated
     private DisputeStatus status;
-
-
 }
--- src/main/java/com/example/beenproject/eneities/enums/BoardStatus.java
+++ src/main/java/com/example/beenproject/eneities/enums/BoardStatus.java
@@ -3,6 +3,11 @@ package com.example.beenproject.eneities.enums;
 import lombok.Getter;
 
 @Getter
+/*
+ACTIVE : 활성화
+DELETED : 유저 삭제
+HIDDEN : 강제 삭제(신고로 인한 삭제)
+ */
 public enum BoardStatus {
     ACTIVE(1),
     DELETED(-1),
